4af1f658194a6ccbe7c6cdfc4d6bf8cffd6ed6ba
Arham Khawar <68909415+ch-Arham@users.noreply.github.com>
2022-09-29 23:41:38 +0500

    Basic set up TODO List

--- App.js
+++ App.js
@@ -1,32 +1,97 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
-  const name = 'John Doe';
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    // This is the best way to update state when it depends on the previous state
+    // as it guarantees that you're always working with the latest state snapshot
+    // and not with a state snapshot that might be outdated
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    // setEnteredGoal('');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.dummyText}>Hello World!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          placeholder="Your Course Goal" 
+          style={styles.textInput}
+          value={enteredGoal}
+          onChangeText={goalInputHandler}
+        />
+        <Button 
+          title="Add Goals" 
+          onPress={addGoalHandler}
+        />
+      </View>
+      
+      <View style={styles.goalsContainer}>
+        {/* <ScrollView alwaysBounceVertical={false}>
+          {courseGoals.map((goal,index) => (
+            <View key={index} style={styles.goalItem}>
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+          ))}
+          
+        </ScrollView> */}
+        <FlatList
+          alwaysBounceVertical={false}
+          data={courseGoals}
+          keyExtractor={(item, index) => index.toString() + Math.random().toString()}
+          renderItem={itemData => (
+            <View style={styles.goalItem}>
+              <Text style={styles.goalText}>{itemData.item}</Text>
+            </View>
+          )}
+        />
+      </View>
 
-      <Text style={styles.dummyText}>My name is {name}</Text>
-
-      <Button title="Click Me" onPress={() => alert('Hello World!')} />
+      <View style={{flex:0.5}}>
+        <Button title="Clear Goals" onPress={() => setCourseGoals([])} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
-  dummyText: {
-    color: 'red',
-    fontSize: 20,
-    fontWeight: 'bold',
-    margin: 16,
-    borderWidth: 3,
-    padding: 16,
-    borderColor: 'blue',
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
+  },
+  textInput: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 8,
+    width: '70%',
+    marginRight: 10
+  },
+  goalsContainer: {
+    flex: 5
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  goalText: {
+    color: '#fff'
   }
 });
