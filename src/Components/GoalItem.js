0b762d4d2318741355c9a1d459511c9ffbba725a
Arham Khawar <68909415+ch-Arham@users.noreply.github.com>
2022-10-09 13:16:36 +0500

    problem fix

--- src/Components/GoalItem.js
+++ src/Components/GoalItem.js
@@ -2,24 +2,28 @@ import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 const GoalItem = ({ text, setCourseGoals, id }) => {
 
+  const handleDelete = () => {
+    setCourseGoals(prevGoals => prevGoals.filter((goal, index) => index.toString() !== id));
+  };
+
   return (
-    <Pressable onPress={() => setCourseGoals(prevGoals => prevGoals.filter((goal, index) => index.toString() !== id))}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable onPress={handleDelete} android_ripple={{color: '#ddd', borderless: false}}>
         <Text style={styles.goalText}>{text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
     goalItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
         backgroundColor: '#5e0acc',
     },
     goalText: {
-        color: '#fff'
+        color: '#fff',
+        padding: 8,
     }
 });
 
